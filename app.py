aaf9441fe80d1b582673643271be873720973ea1
aggreyrc <aggreyronoh@gmail.com>
2024-12-30 15:14:07 +0300

    Added cors

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db,Project
 
@@ -14,6 +15,7 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+CORS(app)
 api = Api(app)
 
 # Home
